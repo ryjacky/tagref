91360c20eabe91053a15ad85f8cba671cb241fb5
sejackye <sejackye@gmail.com>
2022-05-31 23:51:46 +0900

    - Added logo.png - AppBar UI - TagSearchBar.dart optimization: fixed conflicts with Row(widget) parent

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tagref/assets/constant.dart';
 import 'package:tagref/ui/AddButton.dart';
-import 'package:tagref/ui/ReferenceImageDisplay.dart';
 import 'package:tagref/ui/TagSearchBar.dart';
 
 void main() {
@@ -57,26 +57,46 @@ class _TagRefHomePageState extends State<TagRefHome> {
     double paddingH = MediaQuery.of(context).size.width / 5;
 
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: primaryColor,
-        title: Padding(
-          padding: EdgeInsets.symmetric(horizontal: paddingH),
-          child: TagSearchBar(),
-        )
-      ),
-      body: Container(
-        padding: EdgeInsets.fromLTRB(paddingH, 20, paddingH, 0),
-        child: MasonryGridView.count(
-          crossAxisCount: 3,
-          mainAxisSpacing: 15,
-          crossAxisSpacing: 15,
-          itemCount: 100,
-          itemBuilder: (context, index) {
-            return AddButton(onPressed: (){}, imgUrl: "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80",);
-          },
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: primaryColor,
+          title: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/logo.png",
+                width: 30,
+                height: 30,
+                alignment: Alignment.centerLeft,
+              ),
+              Expanded(child: Container()),
+              TagSearchBar(),
+              Expanded(child: Container()),
+              IconButton(
+                icon: const FaIcon(FontAwesomeIcons.bars),
+                alignment: Alignment.centerRight,
+                iconSize: 28,
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
-      )
-    );
+        body: Container(
+          padding: EdgeInsets.fromLTRB(paddingH, 20, paddingH, 0),
+          child: MasonryGridView.count(
+            crossAxisCount: 3,
+            mainAxisSpacing: 15,
+            crossAxisSpacing: 15,
+            itemCount: 100,
+            itemBuilder: (context, index) {
+              return AddButton(
+                onPressed: () {},
+                imgUrl:
+                    "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80",
+              );
+            },
+          ),
+        ));
   }
 }
