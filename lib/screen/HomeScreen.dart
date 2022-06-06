030778194ccdf00832f82c42c0a58a1992a2f115
sejackye <sejackye@gmail.com>
2022-06-06 11:35:56 +0900

    Optimized scrolling performance for large database and reduced setState loops

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
@@ -20,39 +22,58 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final List<String> keywordList = [];
 
-  int gridCounts = 0;
-  final List<Widget> masonryGrids = [];
+  int gridMaxCounts = 50;
+  int currentGridCounts = 0;
+  final masonryUpdateStep = 50;
+
+  final List<Widget> masonryGrids = [
+    AddButton(
+        onPressed: () {},
+        imgUrl:
+            "https://cdn.pixabay.com/photo/2022/05/03/23/35/rapeseed-7172836__340.jpg")
+  ];
+
+  bool isUpdating = true;
 
   // Loads all images into the MasonryGridView and adds an AddButton at last
   Future<void> loadMasonryGrids() async {
     List<Map<String, Object?>> queryResult =
         await DBHelper.db.rawQuery("SELECT src_url FROM images;");
 
+    // Set the roof of gridMaxCounts to the maximum number of query result
+    gridMaxCounts = min(gridMaxCounts, queryResult.length);
+
     // Go through each record from the query result and creates an
     // ReferenceImageDisplay widget which delegates the image for the record
-    if (queryResult.length != gridCounts) {
-      // Runs only when database is updated
-      // !!! This will cause error when the table is modified using !!!
-      // !!! UPDATE command, direct replacement of image is not supported !!!
-      // !!! and redundant to be supported, do not modify the table !!!
-      // !!! directly using UPDATE command !!!
-      setState(() {
-        for (var row in queryResult) {
-          masonryGrids.add(ReferenceImageDisplay(
-            srcUrl: row["src_url"] == null ? "" : row["src_url"].toString(),
-          ));
-        }
-        
-        //Add extra AddButton (Class)
-        masonryGrids.add(AddButton(onPressed: (){}, imgUrl: "https://cdn.pixabay.com/photo/2022/05/03/23/35/rapeseed-7172836__340.jpg"));
-        gridCounts = queryResult.length + 1;
-      });
-    }
+
+    // Only creates 50 widgets per setState()
+    setState(() {
+      gridMaxCounts = min(gridMaxCounts, queryResult.length);
+
+      for (currentGridCounts;
+          currentGridCounts < gridMaxCounts - 1;
+          currentGridCounts++) {
+        masonryGrids.insert(
+            1,
+            ReferenceImageDisplay(
+              srcUrl: queryResult[currentGridCounts]["src_url"] == null
+                  ? ""
+                  : queryResult[currentGridCounts]["src_url"].toString(),
+            ));
+      }
+
+      if (masonryGrids.length == gridMaxCounts){
+        isUpdating = true;
+      }
+
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    loadMasonryGrids();
+    if (masonryGrids.length != gridMaxCounts) {
+      loadMasonryGrids();
+    }
 
     // Calculates the padding from the application window width
     // TODO: Use .w function
@@ -103,16 +124,38 @@ class _HomeScreenState extends State<HomeScreen> {
               keywordList: keywordList,
             ),
             Expanded(
-              child: MasonryGridView.count(
-                crossAxisCount: 3,
-                padding:
-                    EdgeInsets.symmetric(vertical: 20, horizontal: paddingH),
-                mainAxisSpacing: 15,
-                crossAxisSpacing: 15,
-                itemCount: gridCounts,
-                itemBuilder: (context, index) {
-                  return masonryGrids[index];
+              child: NotificationListener<ScrollNotification>(
+                onNotification: (scrollNotification) {
+                  if (scrollNotification.metrics.pixels >=
+                          scrollNotification.metrics.maxScrollExtent - 500 &&
+                      isUpdating) {
+
+                    // set isUpdating to false to prevent calling setState
+                    // more than once
+                    isUpdating = false;
+
+                    // Loading cooldown
+                    Future.delayed(const Duration(seconds: 1), () {
+                      setState(() {
+                        gridMaxCounts += masonryUpdateStep;
+                      });
+                    });
+
+
+                  }
+                  return true;
                 },
+                child: MasonryGridView.count(
+                  crossAxisCount: 3,
+                  padding:
+                      EdgeInsets.symmetric(vertical: 20, horizontal: paddingH),
+                  mainAxisSpacing: 15,
+                  crossAxisSpacing: 15,
+                  itemCount: currentGridCounts,
+                  itemBuilder: (context, index) {
+                    return masonryGrids[index];
+                  },
+                ),
               ),
             )
           ],
